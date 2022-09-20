cb9f208677088971ee7020cc972806c5eb66f6db
diegotur <gpslinea15@hotmail.com>
2022-09-20 13:51:55 -0300

    excel

--- main.js
+++ main.js
@@ -133,10 +133,7 @@ function upload() {
                     result[sheetName] = roa;
                 }
             });
-            //displaying the json result
-            /* var resultEle=document.getElementById("json-result");
-            resultEle.value=JSON.stringify(result, null, 4);
-            resultEle.style.display='block'; */
+          
         }
     }catch(e){
         console.error(e);
@@ -162,7 +159,7 @@ function upload2() {
   let result2 = {};
   let roa2;
 
-  let tableP2 = document.getElementById("tableP");
+  let tableP2 = document.getElementById("tableP2");
    
   //Method to read excel file and convert it into JSON 
   function excelFileToJSON2(file){
@@ -180,7 +177,7 @@ function upload2() {
 
                 for (const elem of roa2){
                     const ExcelDateToJSDate2 = (date) => {
-                        let converted_date = new Date(Math.round((date - 25569) * 864e5));
+                        let converted_date = new Date(Math.round((date - 25568) * 864e5));
                         converted_date = String(converted_date).slice(4, 15);
                         date = converted_date.split(" ");
                         let day = date[1];
@@ -195,6 +192,8 @@ function upload2() {
                     elem.FechaInicio  = ExcelDateToJSDate2(elem.FechaInicio);
                     elem.FechaFin  = ExcelDateToJSDate2(elem.FechaFin);
                 }
+
+                console.log(roa2[1]);
 
                 for (const elem of roa2){
                     function excelDateToJSDate3(excel_date, time = false) {
@@ -219,7 +218,6 @@ function upload2() {
                 for (const elem of roa2){
                     if (fechasDisp.some((n) => n == elem.FechaInicio) == false){
                         fechasDisp.push(elem.FechaInicio);
-                        console.log (elem.FechaInicio);
                     }
                 }
                 fechasDisp.sort((a, b) => (a > b) ? 1 : -1);
@@ -233,128 +231,100 @@ function upload2() {
                     drop[dropIndex].innerText = elem;
                     dropAr.push("drop"+dropIndex);
                     dropIndex++;
-                    //console.log(dropAr);
                 }
 
-
-               
+                              
 
                 for (i=0;i<fechasDisp.length;i++) {
-                    document.getElementById(dropAr[i]).addEventListener("click", () => {Write(drop[i])});
+                    let j = document.getElementById(dropAr[i])
+                    j.addEventListener("click", () => {Write(j.textContent)});
                 }
 
+                let new6=[];
                 function Write(a) {
-                    console.log(a);
-                }
 
 
-
-                let newArray10 = roa2.filter((elem) => elem.FechaInicio != elem.FechaFin);
-
-                let newArray11 = newArray10.filter((elem) => elem.kms > 15);
-
-                //console.log(newArray11);
-
-                roa2 = newArray10;
-
-
-
-
-
-                //console.log(roa2.length);
-                
-                   /*  let roaCortos = roa2.filter((elem) => elem.Secciones < 8);
-                    let roaLargos = roa2.filter((elem) => elem.Secciones > 8);
-
-                    let newArray = roaLargos.filter((elem) => elem.Diferencia > 7);
-                    let newArray1 = newArray.filter((elem) => elem.Ramal < 316);
-                    let newArray2 = newArray1.filter((elem) => elem.Kms > 30);
-                    let newArray6 = roaCortos.filter((elem) => elem.Kms > 12);
-                    let newArray7 = newArray6.filter((elem) => elem.Diferencia > 3);
-
-                    roaCortos = newArray7;
-
-                    for (const elem of newArray2){
-                        roaCortos.push(elem);
-                    }
-
-                let newArray3 = [];
-                for (const elem of roaCortos){
-
-                    let result = (({ Interno, Ramal, Legajo, Kms, Secciones, Chofer, Diferencia }) => ({ Interno, Ramal, Legajo, Kms, Secciones, Chofer, Diferencia }))(elem);
-
-                    newArray3.push(result);
-                }
-                roa2 = newArray3;          
-
-                roa2.sort((a, b) => (a.Legajo > b.Legajo) ? 1 : -1);     
-                
-                for (const elem of roa2){
-                    const node = document.createElement("tr");
-                    const subNode = document.createElement("td");
-                    const subNode1 = document.createElement("td");
-                    const subNode2 = document.createElement("td");
-                    const subNode3 = document.createElement("td");
-                    const subNode4 = document.createElement("td");
-                    const subNode5 = document.createElement("td");
-                    const subNode6 = document.createElement("td");
-
-                    const textnode = document.createTextNode(elem.Interno);
-                    const textnode1 = document.createTextNode(elem.Ramal);
-                    const textnode2 = document.createTextNode(elem.Legajo);
-                    const textnode3 = document.createTextNode(elem.Kms);
-                    const textnode4 = document.createTextNode(elem.Secciones);
-                    const textnode5 = document.createTextNode(elem.Chofer);
-                    const textnode6 = document.createTextNode(elem.Diferencia);
-                    subNode.appendChild(textnode);
-                    subNode1.appendChild(textnode1);
-                    subNode2.appendChild(textnode2);
-                    subNode3.appendChild(textnode3);
-                    subNode4.appendChild(textnode4);
-                    subNode5.appendChild(textnode5);
-                    subNode6.appendChild(textnode6);
-                    node.appendChild(subNode);
-                    node.appendChild(subNode1);
-                    node.appendChild(subNode2);
-                    node.appendChild(subNode3);
-                    node.appendChild(subNode4);
-                    node.appendChild(subNode5);
-                    node.appendChild(subNode6);
-                    tableP2.appendChild(node);
-
-                   
-                } */
-                /* for (const elem of roa){
-                    const ExcelDateToJSDate = (date) => {
-                        let converted_date = new Date(Math.round((date - 25569) * 864e5));
-                        converted_date = String(converted_date).slice(4, 15);
-                        date = converted_date.split(" ");
-                        let day = date[1];
-                        let month = date[0];
-                        month = "JanFebMarAprMayJunJulAugSepOctNovDec".indexOf(month) / 3 + 1;
-                        if (month.toString().length <= 1){
-                            month = '0' + month;
-                        }
-                        let year = date[2];
-                        return String(day + '/' + month + '/' + year.slice(2, 4));
-                    }
-                    elem.FECHA  = ExcelDateToJSDate(elem.FECHA);
                     
-                    /* roa.sort((a, b) => (a.FECHA > b.FECHA) ? 1 : -1); */
+                    
+                    let newArray10 = roa2.filter((elem) => a == elem.FechaInicio);
+                    let newPP = newArray10.filter((elem) => a !== elem.FechaFin);
+                    let newPPP = newPP.filter((elem) => elem.HoraFin > "02:00:00");
+                    let newArray11 = newPPP.filter((elem) => elem.kms > 15);
 
-                  /*  delete elem.RECA;
+                    let newArray20 = newArray10.filter((elem) => a == elem.FechaFin);
+                    let kmDeMas63 = newArray20.filter((elem) => elem.kms > "63");
+                    let pcoMasde48 = newArray20.filter((elem) => elem.kms > "48" && elem.Recorrido == "PCO COM");
+                    let pcoRapMasde48 = newArray20.filter((elem) => elem.kms > "48" && elem.Recorrido == "PCO RAP");
+                    let fonMasde54 = newArray20.filter((elem) => elem.kms > "54" && elem.Recorrido == "FON COM");
+                    let fonRapMasde54 = newArray20.filter((elem) => elem.kms > "54" && elem.Recorrido == "FON RAP");
 
+                    let new2 = newArray11.concat(kmDeMas63);
+                    let new3 =new2.concat(pcoMasde48);
+                    let new4 =new3.concat(pcoRapMasde48);
+                    let new5 =new4.concat(fonMasde54);
+                    new6 =new5.concat(fonRapMasde54);
 
-
-                } */
-                /* roa.shift();
-                for (elem of roa){
-                    delete elem.__EMPTY_5;
-                    if (elem.__EMPTY_12 < 4){
-                        delete elem.__EMPTY_12;
+                    
+                    roa2 = new6;          
+                    
+                    roa2.sort((a, b) => (a.Legajo > b.Legajo) ? 1 : -1);     
+                    
+                    for (const elem of roa2){
+                        const node = document.createElement("tr");
+                        const subNode = document.createElement("td");
+                        const subNode1 = document.createElement("td");
+                        const subNode2 = document.createElement("td");
+                        const subNode3 = document.createElement("td");
+                        const subNode4 = document.createElement("td");
+                        const subNode5 = document.createElement("td");
+                        const subNode6 = document.createElement("td");
+                        const subNode7 = document.createElement("td");
+                        
+                        const textnode = document.createTextNode(elem.Legajo);
+                        const textnode1 = document.createTextNode(elem.Interno);
+                        const textnode2 = document.createTextNode(elem.FechaInicio);
+                        const textnode3 = document.createTextNode(elem.FechaFin);
+                        const textnode4 = document.createTextNode(elem.HoraInicio);
+                        const textnode5 = document.createTextNode(elem.HoraFin);
+                        const textnode6 = document.createTextNode(elem.Recorrido);
+                        const textnode7 = document.createTextNode(elem.kms);
+                        subNode.appendChild(textnode);
+                        subNode1.appendChild(textnode1);
+                        subNode2.appendChild(textnode2);
+                        subNode3.appendChild(textnode3);
+                        subNode4.appendChild(textnode4);
+                        subNode5.appendChild(textnode5);
+                        subNode6.appendChild(textnode6);
+                        subNode7.appendChild(textnode7);
+                        node.appendChild(subNode);
+                        node.appendChild(subNode1);
+                        node.appendChild(subNode2);
+                        node.appendChild(subNode3);
+                        node.appendChild(subNode4);
+                        node.appendChild(subNode5);
+                        node.appendChild(subNode6);
+                        node.appendChild(subNode7);
+                        tableP2.appendChild(node);
+                        
                     }
+                    //PARA CUANDO SE ARREGLE EL TEMA DE LA DIFERENCIA DE HORARIO
+                    /* let masHoras = [];
+                    for (const elem of newArray20){
 
-                } */
+                        let s = elem.HoraInicio.split(':');
+                        let e = elem.HoraFin.split(':');
+                        
+                        let tiempo = e[0] - s[0];
+
+                        if (tiempo > "6"==true){
+                            masHoras.push(elem);
+                        }
+                    }
+                    console.log(masHoras);
+                        
+                    } */
+
+                }
                 if (roa2.length > 0) {
 
                     result[sheetName] = roa2;
